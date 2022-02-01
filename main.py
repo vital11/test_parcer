f9bb71840874799b0defc5fcec8082e43418198e
vital11 <1861755@gmail.com>
2022-02-01 16:17:10 +0200

    fix hardcoded paths

--- main.py
+++ main.py
@@ -83,38 +83,35 @@ def download_business_case_pdf(html: str, url: str) -> None:
     soup = BeautifulSoup(html, 'lxml')
     links = [url + link.get('href').lstrip('/') for link in soup.find_all('a')]
     for link in links:
-        file_name = link.split('/')[-1].rstrip("'")
-        file_system_lib.remove_file(f'output/{file_name}.pdf', missing_ok=True)
+        file_name = link.split('/')[-1].rstrip("'") + '.pdf'
+        path = os.path.join('output', file_name)
+        file_system_lib.remove_file(path, missing_ok=True)
         browser_lib.go_to(link)
         browser_lib.wait_until_page_contains_element('link:Download Business Case PDF')
         browser_lib.click_element_if_visible('link:Download Business Case PDF')
-        file_system_lib.wait_until_created(path=f'output/{file_name}.pdf', timeout=20.0)
-        logger.debug(f"File {file_name}.pdf was downloaded.")
+        file_system_lib.wait_until_created(path, timeout=20.0)
+        logger.debug(f"File {file_name} was downloaded.")
         browser_lib.go_back()
 
 
-def remove_the_duplicate_files_from_the_folder() -> None:
-    file_path = os.path.abspath('output/')
-    file_list = os.listdir(file_path)
+def remove_the_duplicate_files_from_the_folder(folder: str) -> None:
+    file_list = os.listdir(folder)
     for file_name in file_list:
         if 'excel' in file_name:
             continue
         if "pdf" not in file_name:
-            os.remove(os.path.join(file_path, file_name))
-        pattern = r' \([0-9]+\).pdf'
+            os.remove(os.path.join(folder, file_name))
+        pattern = r'\([0-9]+\).pdf'
         if re.search(pattern, file_name) is None:
             continue
-        original_file_name = re.sub(pattern=pattern, repl='', string=file_name)
-        if not os.path.exists(os.path.join(file_path, original_file_name)):
-            continue  # do not remove files which have no original
-        os.remove(os.path.join(file_path, file_name))
+        os.remove(os.path.join(folder, file_name))
 
 
 def extract_data_from_pdf() -> List:
-    file_list = [file_name for file_name in os.listdir('output/') if 'excel' not in file_name]
+    file_list = [file_name for file_name in os.listdir('output') if 'excel' not in file_name]
     pdf_values = []
     for file_name in file_list:
-        page = PDF().get_text_from_pdf(source_path=f'output/{file_name}', pages=1).get(1)
+        page = PDF().get_text_from_pdf(source_path=os.path.join('output', file_name), pages=1).get(1)
         values = page.replace(f'\n', ' ').split(
             'Name of this Investment: ')[-1].split(
             "Section B")[0].split(
@@ -139,21 +136,23 @@ def compare_values(pdf_values: list, content: Optional[Table]) -> None:
 
 def main():
     url = 'https://itdashboard.gov/'
-    file_system_lib.create_directory(path='output/', exist_ok=True)
-    browser_lib.set_download_directory(directory=os.path.abspath('output/'), download_pdf=True)
+    file_system_lib.create_directory('output', exist_ok=True)
+    output = os.path.abspath('output')
+    excel_path = os.path.join('output', 'excel.xlsx')
+    browser_lib.set_download_directory(output, download_pdf=True)
     browser_lib.open_available_browser(url)
     logger.add("log.log", format="{time} {level} {message}", level="INFO",
                rotation="1 MB", compression="zip")
 
     try:
         content = get_amounts_for_each_agency()
-        write_excel_worksheet_agencies('output/excel.xlsx', 'Agencies', content)
+        write_excel_worksheet_agencies(excel_path, 'Agencies', content)
         select_one_of_the_agencies(agency, url)
         html = get_agency_individual_investments_table()
         content = scrape_agency_individual_investments_table(html)
-        add_excel_worksheet_table('output/excel.xlsx', 'Table', content)
+        add_excel_worksheet_table(excel_path, 'Table', content)
         download_business_case_pdf(html, url)
-        remove_the_duplicate_files_from_the_folder()
+        remove_the_duplicate_files_from_the_folder(output)
         pdf_values = extract_data_from_pdf()
         compare_values(pdf_values, content)
     finally:
@@ -162,3 +161,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
